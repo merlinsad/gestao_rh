4ddc717d28bf85148ec5b5dc9fe2db31b606b58d
Vinicius Vicenzo <vvprockl@outlook.com>
2022-01-09 12:05:44 -0300

    Att

--- app/documentos/models.py
+++ app/documentos/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from app.funcionarios.models import Funcionario
 
 
 # Create your models here.
 class Documento(models.Model):
     descricao = models.CharField(max_length=100, help_text='Nome do documento')
+    pertence = models.ForeignKey(
+        Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
--- app/funcionarios/models.py
+++ app/funcionarios/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from app.departamentos.models import Departamento
+from app.empresas.models import Empresa
 
 
-# Create your models here.
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome do funcionario')
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
