f09dbb3e820acaeb21097fe2a79ca6e6d1745b74
Vinicius Vicenzo <vvprockl@outlook.com>
2022-01-13 06:30:14 -0300

    Att

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import render
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Ola')
+    return render(request, 'core/index.html')
