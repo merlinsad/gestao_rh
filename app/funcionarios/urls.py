408bce23b621ac042bf23fcf0bab5cf89ca6a63f
Vinicius Vicenzo <vvprockl@outlook.com>
2022-01-22 10:00:26 -0300

    att

--- app/funcionarios/urls.py
+++ app/funcionarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete
+from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete, FuncionarioNovo
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
+    path('novo/', FuncionarioNovo.as_view(), name='create_funcionarios'),
     path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update_funcionarios'),
     path('delete/<int:pk>/', FuncionarioDelete.as_view(), name='delete_funcionarios')
 ]
