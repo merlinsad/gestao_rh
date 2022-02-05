fb126ac8945b0db833c494a493f84c51c83612c1
Vinicius Vicenzo <vvprockl@outlook.com>
2022-02-04 23:45:31 -0300

    Atualização no Index

--- app/core/templates/core/teste.js
+++
@@ -1,7 +0,0 @@
-function sortByPriceAscending(jsonString) {
-
-  jsonString.sort((a, b) => Number(a.price) - Number(b.price));
-  console.log(jsonString);
-}
-
-console.log(sortByPriceAscending([{"name":"eggs","price":1} + {"name":"coffee","price":9.99} + {"name":"rice","price":4.04}]))
